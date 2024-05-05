121457c85a7cc8ad91f7dc9e155ae49bdc4b44f4
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-05-05 16:07:40 +0700

    add filter usulan on superadmin

--- Models/Silastri/Adm/Layanan/SelesaiModel.php
+++ Models/Silastri/Adm/Layanan/SelesaiModel.php
@@ -56,7 +56,7 @@ class SelesaiModel extends Model
         // $this->dt->select("a.id as id_usulan, a.date_approve, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan, a.date_matching, a.date_terbitsk");
         // $this->dt->join('_ptk_tb b', 'a.id_ptk = b.id');
         // $this->dt->whereIn('a.status_usulan', [6]);
-        $this->dt->whereIn('a.layanan', $layanan);
+        $this->dt->where('a.layanan', $layanan);
         if ($this->request->getPost('layanan')) {
             if ($this->request->getPost('layanan') !== "") {
 
@@ -75,7 +75,7 @@ class SelesaiModel extends Model
         // $this->dt->select("a.id as id_usulan, a.date_approve, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan, a.date_matching, a.date_terbitsk");
         // $this->dt->join('_ptk_tb b', 'a.id_ptk = b.id');
         // $this->dt->whereIn('a.status_usulan', [6]);
-        $this->dt->whereIn('a.layanan', $layanan);
+        $this->dt->where('a.layanan', $layanan);
         if ($this->request->getPost('layanan')) {
             if ($this->request->getPost('layanan') !== "") {
 
@@ -92,7 +92,7 @@ class SelesaiModel extends Model
         // $this->dt->select("a.id as id_usulan, a.date_approve, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan, a.date_matching, a.date_terbitsk");
         // $this->dt->join('_ptk_tb b', 'a.id_ptk = b.id');
         // $this->dt->whereIn('a.status_usulan', [6]);
-        $this->dt->whereIn('a.layanan', $layanan);
+        $this->dt->where('a.layanan', $layanan);
         if ($this->request->getPost('layanan')) {
             if ($this->request->getPost('layanan') !== "") {
 
