af7bc39fcc1242d95dd902511f723de8b9fd4f15
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-05-06 17:33:25 +0700

    add export file adm

--- Views/silastri/adm/layanan/selesai/download.php
+++ Views/silastri/adm/layanan/selesai/download.php
@@ -35,13 +35,13 @@
                     <div class="mt-4">
                         <h5 class="font-size-14 mb-4">Format Download</h5>
                         <div class="form-check mb-3">
-                            <input class="form-check-input" type="radio" value="pdf" name="_type_file" id="formRadios1" checked="">
+                            <input class="form-check-input" type="radio" value="pdf" name="_type_file" id="formRadios1">
                             <label class="form-check-label" for="formRadios1">
                                 PDF
                             </label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" value="xlsx" name="_type_file" id="formRadios2">
+                            <input class="form-check-input" type="radio" value="xlsx" name="_type_file" id="formRadios2" checked="">
                             <label class="form-check-label" for="formRadios2">
                                 Excel
                             </label>
