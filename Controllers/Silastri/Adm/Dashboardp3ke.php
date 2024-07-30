f18539e498e1a6cc4b38e25cb2481742d6c0ecc5
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-07-30 17:47:44 +0700

    updated menu

--- Controllers/Silastri/Adm/Dashboardp3ke.php
+++ Controllers/Silastri/Adm/Dashboardp3ke.php
@@ -322,6 +322,25 @@ class Dashboardp3ke extends BaseController
                         $response->data = view('silastri/adm/dashboardp3ke/statistik_rekap_p3ke_resiko_stunting', $x);
                         return json_encode($response);
                         break;
+                    case 'status_perkawinan':
+                        $result = $this->_db->table('ref_p3ke_individu a')
+                            ->select("status_kawin, count(status_kawin) as jumlah")
+                            ->groupBy('status_kawin')
+                            ->get()
+                            ->getResult();
+
+                        $totalData = array_reduce($result, function ($carry, $item) {
+                            return $carry + $item->jumlah;
+                        }, 0);
+
+                        $x['data'] = $result;
+                        $x['total_data'] = $totalData;
+                        $response = new \stdClass;
+                        $response->status = 200;
+                        $response->message = "dikenali.";
+                        $response->data = view('silastri/adm/dashboardp3ke/statistik_rekap_p3ke_status_kawin', $x);
+                        return json_encode($response);
+                        break;
 
                     default:
                         $response = new \stdClass;
