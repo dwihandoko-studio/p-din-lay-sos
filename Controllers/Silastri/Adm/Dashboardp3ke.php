8f8501b8c79f3dc8cb699dd47fd832aedfa3314d
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-07-30 17:32:48 +0700

    updated menu

--- Controllers/Silastri/Adm/Dashboardp3ke.php
+++ Controllers/Silastri/Adm/Dashboardp3ke.php
@@ -246,6 +246,25 @@ class Dashboardp3ke extends BaseController
                         $response->data = view('silastri/adm/dashboardp3ke/statistik_rekap_p3ke_pendidikan', $x);
                         return json_encode($response);
                         break;
+                    case 'jenis_dinding':
+                        $result = $this->_db->table('ref_p3ke_keluarga a')
+                            ->select("jenis_dinding, count(jenis_dinding) as jumlah")
+                            ->groupBy('jenis_dinding')
+                            ->get()
+                            ->getResult();
+
+                        $totalData = array_reduce($result, function ($carry, $item) {
+                            return $carry + $item->jumlah;
+                        }, 0);
+
+                        $x['data'] = $result;
+                        $x['total_data'] = $totalData;
+                        $response = new \stdClass;
+                        $response->status = 200;
+                        $response->message = "dikenali.";
+                        $response->data = view('silastri/adm/dashboardp3ke/statistik_rekap_p3ke_jenis_dinding', $x);
+                        return json_encode($response);
+                        break;
 
                     default:
                         $response = new \stdClass;
