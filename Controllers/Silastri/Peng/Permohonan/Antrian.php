c6ce0a88c6c947c8caa62b5bf3663ee69f349f6a
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-12-02 21:55:19 +0700

    add print permohonan layanan

--- Controllers/Silastri/Peng/Permohonan/Antrian.php
+++ Controllers/Silastri/Peng/Permohonan/Antrian.php
@@ -11,6 +11,7 @@ use App\Libraries\Profilelib;
 use App\Libraries\Apilib;
 use App\Libraries\Helplib;
 use App\Libraries\Uuid;
+use Dompdf\Dompdf;
 
 class Antrian extends BaseController
 {
@@ -228,8 +229,160 @@ class Antrian extends BaseController
             ->where(['a.id' => $id, 'a.status_permohonan' => 0])->get()->getRowObject();
 
         if ($current) {
-            $data['data'] = $current;
-            return view('silastri/peng/permohonan/print', $data);
+
+            $dataFileGambar = file_get_contents(FCPATH . './favicon/android-icon-144x144.png');
+            $base64 = "data:image/png;base64," . base64_encode($dataFileGambar);
+
+            $qrCode = "data:image/png;base64," . base64_encode(file_get_contents('http://192.168.33.16:8020/generate?data=' . base_url() . '/verifiqrcode?token=' . $current->id));
+
+            $html   =  '<html>
+                        <head>
+                            <link href="';
+            $html   .=              base_url() . '/assets/css/bootstrap.min.css';
+            $html   .=          '" rel="stylesheet">
+                        </head>
+                        <body>
+                            <div class="container">
+                                <div class="row">
+                                    <table class="table table-responsive" style="border: none;">
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <img class="image-responsive" width="110px" height="110px" src="';
+            $html   .=                                      $base64;
+            $html   .=                                  '"/>
+                                                </td>
+                                                <td>
+                                                    &nbsp;&nbsp;&nbsp;
+                                                </td>
+                                                <td>
+                                                    <h3 style="margin: 0rem;font-size: 16px; font-weight: 500;">PEMERINTAH KABUPATEN LAMPUNG TENGAH</h3>
+                                                    <h3 style="margin: 0rem;font-size: 16px; font-weight: 500;">DINAS SOSIAL</h3>
+                                                    <h3 style="margin: 0rem;font-size: 16px; font-weight: 500;">KABUPATEN LAMPUNG TENGAH</h3>
+                                                    <h3 style="margin: 0rem;font-size: 16px; font-weight: 500;">Jl. Raya Padang Ratu No. 01 Komplek Perkantoran Pemerintah Daerah Gunung Sugih, Kabupaten Lampung Tengah Provinsi Lampung Kode Pos: 34161</h3>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="row">
+                                    <div style="text-align: center;margin-top: 30px; border:1px solid black;">
+                                        <h3 style="margin: 0rem;font-size: 14px;">KARTU TANDA DAFTAR LAYANAN</h3>
+                                        <h3 style="margin: 0rem;font-size: 14px;">USULAN PENCAIRAN TUNJANGAN PROFESI GURU (TPG)</h3>
+                                        <h3 style="margin: 0rem;font-size: 12px;">NOMOR: ';
+            $html   .=                          $current->kode_permohonan;
+            $html   .=                          '</h3>
+                                    </div>
+                                </div>
+                                <div style="max-width: 100%; padding-left: 10px; padding-right: 8px;">
+                                    <table width="100%" style="border: solid #cbd4dd; font-size: 12px">
+                                        <tbody>
+                                            <tr>
+                                                <td width="35%" align="" style="padding-left: 10px;">Kode Permohonan</td>
+                                                <td width="5%" align="center">:</td>
+                                                <td width="60%" align="left"><?= $data->kode_permohonan ?></td>
+                                                <td rowspan="7" style="border: none" width="10%">
+                                                    &nbsp;
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="" style="padding-left: 10px;">Nama Lengkap</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= str_replace('&#039;', "`", str_replace("'", "`", $data->nama)) ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="" style="padding-left: 10px;">NIK</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= $data->nik ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="" style="padding-left: 10px;">Tempat Lahir</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= $data->tempat_lahir ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="" style="padding-left: 10px;">Tanggal Lahir</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= tgl_indo2($data->tgl_lahir) ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="" style="padding-left: 10px;">Jenis Kelamin</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= ($data->jenis_kelamin == 'L') ? "Laki-Laki" : "Perempuan"; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="" style="padding-left: 10px;">No Handphone</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= $data->no_hp ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="" style="padding-left: 10px;">Email</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= $data->email ?></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div style="max-width: 100%; padding-top: 5px; padding-left: 10px; padding-right: 8px;">
+                                    <table width="100%" style="border: solid #cbd4dd; font-size: 12px">
+                                        <tbody>
+                                            <tr>
+                                                <td colspan="5" align="left">&nbsp;&nbsp;&nbsp;<b>Daftar Layanan</b></td>
+                                                <td rowspan="6" style="border: none" width="10%">
+                                                    <img class="img" src="http://192.168.33.16:8020/generate?data=<?= base_url() ?>/verifiqrcode?token=<?= $data->id ?>" ec="H" style="width: 20mm; background-color: white; color: black;">
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td width="5%"></td>
+                                                <td width="30%" align="">Kode Permohonan</td>
+                                                <td width="5%" align="center">:</td>
+                                                <td width="60%" align="left">
+                                                    <?= $data->kode_permohonan ?>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td></td>
+                                                <td align="">Nama Layanan</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= $data->layanan ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td></td>
+                                                <td align="">Jenis Layanan</td>
+                                                <td align="center">:</td>
+                                                <td align="left"><?= $data->jenis ?></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </body>
+                    </html>';
+
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml($html);
+            $dompdf->setPaper('A4', 'potrait');
+            $dompdf->render();
+            // $dompdf->output();
+            // $m->addRaw($dompdf->output());
+            // unset($dompdf);
+
+            // $m->addRaw($dompdf1->output());
+
+            $dir = FCPATH . "upload/generate/pendaftaran";
+            $fileNya = $dir . '/' . $current->kode_permohonan . '.pdf';
+
+            file_put_contents($fileNya, $dompdf->output());
+
+            sleep(3);
+
+            header('Content-Type: application/pdf');
+            header('Content-Disposition: attachment; filename="' . basename($fileNya) . '"');
+            header('Content-Length: ' . filesize($fileNya));
+            readfile($fileNya);
+
+            return;
+            // return view('silastri/peng/permohonan/print', $data);
         } else {
             return view('404');
         }
