0644ae79397d4c30e7abbb31424734c10483b7db
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-01-14 15:21:34 +0700

    add new assesment

--- Views/silastri/peksos/assesment/antrian/form-asesment-ppks-aduan-new.php
+++ Views/silastri/peksos/assesment/antrian/form-asesment-ppks-aduan-new.php
@@ -397,9 +397,9 @@
                 <hr style="margin-top: 10px;" />
                 <h4>Kondisi PPKS</h4>
                 <div class="col-lg-12 mb-2 mt-2">
-                    <table border="1">
+                    <table border="1" width="100%">
                         <tr>
-                            <td>
+                            <td width="40%">
                                 <div class="col-lg-12 mb-2 mt-2">
                                     <label class="form-label">Group PPKS:</label>
                                     <select class="form-control select2 group_ppks_1" id="_group_ppks_1" name="_group_ppks[]" style="width: 100%">
@@ -415,7 +415,7 @@
                                     <div class="help-block _group_ppks_1"></div>
                                 </div>
                             </td>
-                            <td>
+                            <td width="40%">
                                 <div class="col-lg-12 mb-2 mt-2">
                                     <label class="form-label">Kategori PPKS:</label>
                                     <select class="form-control select2 _kategori_ppks_1" id="_kategori_ppks_1" name="_kategori_ppks[]" style="width: 100%">
@@ -424,7 +424,7 @@
                                     <div class="help-block _kategori_ppks_1"></div>
                                 </div>
                             </td>
-                            <td>
+                            <td width="20%">
                                 <button type="button" class="btn btn-primary waves-effect waves-light">
                                     <i class="fas fa-plus font-size-16 align-middle me-2"></i> Tambah
                                 </button>
