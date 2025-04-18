7dd013a3a4b4f38d26d1f74a200a69a0aed05fb2
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-04-18 19:56:38 +0700

    pembenahan ditolak adm pengaduan

---
+++ Controllers/Silastri/Adm/Pengaduan/Ditolak.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace App\Controllers\Silastri\Adm\Pengaduan;
+
+use App\Controllers\BaseController;
+use App\Models\Silastri\Adm\Pengaduan\DitolakModel;
+use Config\Services;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use App\Libraries\Profilelib;
+use App\Libraries\Apilib;
+use App\Libraries\Helplib;
+use App\Libraries\Uuid;
+use App\Libraries\Silastri\Riwayatpengaduanlib;
+use App\Libraries\Silastri\Notificationlib;
+use iio\libmergepdf\Merger;
+use Dompdf\Dompdf;
+
+class Ditolak extends BaseController
+{
+    var $folderImage = 'masterdata';
+    private $_db;
+    private $model;
+    private $_helpLib;
+
+    function __construct()
+    {
+        helper(['text', 'file', 'form', 'session', 'array', 'imageurl', 'web', 'filesystem']);
+        $this->_db      = \Config\Database::connect();
+        $this->_helpLib = new Helplib();
+    }
+
+    public function getAll()
+    {
+        $request = Services::request();
+        $datamodel = new DitolakModel($request);
+
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->status != 200) {
+            session()->destroy();
+            delete_cookie('jwt');
+            return redirect()->to(base_url('auth'));
+        }
+
+        $bidangs = getBidangNaungan($user->data->id);
+
+        $lists = $datamodel->get_datatables($bidangs);
+        $data = [];
+        $no = $request->getPost("start");
+        foreach ($lists as $list) {
+            $no++;
+            $row = [];
+
+            $row[] = $no;
+            // $action = '<div class="btn-group">
+            //             <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Action <i class="mdi mdi-chevron-down"></i></button>
+            //             <div class="dropdown-menu" style="">
+            //                 <a class="dropdown-item" href="javascript:actionDetail(\'' . $list->id . '\', \'' . str_replace("'", "", $list->nama) . '\');"><i class="bx bxs-show font-size-16 align-middle"></i> &nbsp;Detail</a>
+            //                 <a class="dropdown-item" href="javascript:actionSync(\'' . $list->id . '\', \'' . $list->id_ptk . '\', \'' . str_replace("'", "", $list->nama)  . '\', \'' . $list->nuptk  . '\', \'' . $list->npsn . '\');"><i class="bx bx-transfer-alt font-size-16 align-middle"></i> &nbsp;Sync Dapodik</a>
+            //             </div>
+            //         </div>';
+            $action = '<a href="javascript:actionDetail(\'' . $list->id . '\', \'' . $list->nik . '\', \'' . str_replace('&#039;', "`", str_replace("'", "`", $list->nama)) . '\');"><button type="button" class="btn btn-primary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+                <i class="bx bxs-show font-size-16 align-middle"></i> DETAIL</button>
+                </a>';
+            //     <a href="javascript:actionSync(\'' . $list->id . '\', \'' . $list->id_ptk . '\', \'' . str_replace("'", "", $list->nama)  . '\', \'' . $list->nuptk  . '\', \'' . $list->npsn . '\');"><button type="button" class="btn btn-secondary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+            //     <i class="bx bx-transfer-alt font-size-16 align-middle"></i></button>
+            //     </a>
+            //     <a href="javascript:actionHapus(\'' . $list->id . '\', \'' . str_replace("'", "", $list->nama)  . '\', \'' . $list->nuptk . '\');" class="delete" id="delete"><button type="button" class="btn btn-danger btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+            //     <i class="bx bx-trash font-size-16 align-middle"></i></button>
+            //     </a>';
+            $row[] = $action;
+            $row[] = $list->kategori;
+            $row[] = $list->kode_aduan;
+            $row[] = $list->nik;
+            $row[] = str_replace('&#039;', "`", str_replace("'", "`", $list->nama));
+            $row[] = str_replace('&#039;', "`", str_replace("'", "`", $list->nama_aduan));
+
+            $data[] = $row;
+        }
+        $output = [
+            "draw" => $request->getPost('draw'),
+            "recordsTotal" => $datamodel->count_all($bidangs),
+            "recordsFiltered" => $datamodel->count_filtered($bidangs),
+            "data" => $data
+        ];
+        echo json_encode($output);
+    }
+
+    public function index()
+    {
+        return redirect()->to(base_url('silastri/adm/pengaduan/ditolak/data'));
+    }
+
+    public function data()
+    {
+        $data['title'] = 'Pengaduan Layanan Ditolak';
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->status != 200) {
+            session()->destroy();
+            delete_cookie('jwt');
+            return redirect()->to(base_url('auth'));
+        }
+
+        $data['user'] = $user->data;
+
+        $data['jeniss'] = ['Pengaduan Program Bantuan Sosial', 'Pengaduan Pemerlu Pelayanan Kesejahteraan Sosial (PPKS)', 'Pengaduan Layanan Sosial', 'Lainnya'];
+
+        return view('silastri/adm/pengaduan/ditolak/index', $data);
+    }
+
+    public function detail()
+    {
+        if ($this->request->getMethod() != 'post') {
+            $response = new \stdClass;
+            $response->status = 400;
+            $response->message = "Permintaan tidak diizinkan";
+            return json_encode($response);
+        }
+
+        $rules = [
+            'id' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'Id tidak boleh kosong. ',
+                ]
+            ],
+            'nik' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'NIK tidak boleh kosong. ',
+                ]
+            ],
+            'nama' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'Nama tidak boleh kosong. ',
+                ]
+            ],
+        ];
+
+        if (!$this->validate($rules)) {
+            $response = new \stdClass;
+            $response->status = 400;
+            $response->message = $this->validator->getError('id')
+                . $this->validator->getError('nik')
+                . $this->validator->getError('nama');
+            return json_encode($response);
+        } else {
+            $id = htmlspecialchars($this->request->getVar('id'), true);
+            $nik = htmlspecialchars($this->request->getVar('nik'), true);
+            $nama = htmlspecialchars($this->request->getVar('nama'), true);
+
+            $Profilelib = new Profilelib();
+            $user = $Profilelib->user();
+            if ($user->status != 200) {
+                session()->destroy();
+                delete_cookie('jwt');
+                $response = new \stdClass;
+                $response->status = 401;
+                $response->message = "Session telah habis";
+                return json_encode($response);
+            }
+
+            $current = $this->_db->table('_pengaduan a')
+                ->select("a.*")
+                // ->join('_profil_users_tb b', 'b.id = a.user_id')
+                ->join('ref_kecamatan c', 'c.id = a.kecamatan')
+                ->join('ref_kelurahan d', 'd.id = a.kelurahan')
+                ->where(['a.id' => $id, 'a.status_aduan' => 5])->get()->getRowObject();
+
+            if ($current) {
+                $granted = grantedBidangNaungan($user->data->id, $current->diteruskan_ke);
+                if ($granted) {
+                    $data['data'] = $current;
+                    $response = new \stdClass;
+                    $response->status = 200;
+                    $response->message = "Permintaan diizinkan";
+                    $response->data = view('silastri/adm/pengaduan/ditolak/detail', $data);
+                    return json_encode($response);
+                } else {
+                    $response = new \stdClass;
+                    $response->status = 400;
+                    $response->message = "Akses tidak dizinkan.";
+                    return json_encode($response);
+                }
+            } else {
+                $response = new \stdClass;
+                $response->status = 400;
+                $response->message = "Data tidak ditemukan";
+                return json_encode($response);
+            }
+        }
+    }
+}
