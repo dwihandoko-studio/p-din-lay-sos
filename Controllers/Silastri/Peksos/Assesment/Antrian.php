f0df17022b83b644e63e282d2f40183abc56d1be
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-04-20 13:47:10 +0700

    pembenahan ditolak adm pengaduan

--- Controllers/Silastri/Peksos/Assesment/Antrian.php
+++ Controllers/Silastri/Peksos/Assesment/Antrian.php
@@ -1480,6 +1480,13 @@ class Antrian extends BaseController
                 return json_encode($response);
             }
 
+            $response = new \stdClass;
+            $response->status = 400;
+            $response->err = $identifikasi_kebutuhan;
+            $response->message = "vardump.";
+            return json_encode($response);
+
+
             $refUraianKebutuhan = $this->_db->table('ref_uraian_kebutuhan_layanan')
                 ->select("CONCAT(kebutuhan_layanan, ' (Rencana Interversi: ', rencana_intervensi, ')') as uraian_kebutuhan")
                 ->whereIn('id', $identifikasi_kebutuhan)
