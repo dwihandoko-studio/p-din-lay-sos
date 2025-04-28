ce9cdb7150b74135b200f49bd749c37201681d13
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-04-29 00:33:35 +0700

    fix verifi qrcode

--- Views/silastri/kepala/layanan/antrian/detail.php
+++ Views/silastri/kepala/layanan/antrian/detail.php
@@ -34,7 +34,12 @@
                 <label class="col-form-label">Layanan:</label>
                 <input type="text" class="form-control" value="<?= $data->layanan ?>" readonly />
             </div>
-
+            <?php if (isset($data->to_rumah_sakit)) { ?>
+                <div class="col-lg-6">
+                    <label class="col-form-label">Tujuan Rumah Sakit:</label>
+                    <input type="text" class="form-control" value="<?= $data->to_rumah_sakit ?>" readonly />
+                </div>
+            <?php } ?>
             <?php if (isset($data->lampiran_ktp)) { ?>
                 <div class="col-lg-12 mt-2">
                     <label class="col-form-label">Lampiran Dokumen:</label>
