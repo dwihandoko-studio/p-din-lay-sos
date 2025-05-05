ae2d1bcbb12b177172731c5396323eb7972f8032
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-05-06 02:07:24 +0700

    fix modul layanan rppbkks

--- Views/silastri/peng/riwayat/pengaduan/detail.php
+++ Views/silastri/peng/riwayat/pengaduan/detail.php
@@ -94,6 +94,14 @@
                                 <input type="text" class="form-control" value="<?= $data->kategori ?>" readonly />
                             </div>
 
+                            <?php if (isset($data->keterangan_reject)) {
+                                if (!($data->keterangan_reject === "" || $data->keterangan_reject === NULL)) { ?>
+                                    <div class="col-lg-12">
+                                        <label class="col-form-label">Keterangan Penolakan:</label>
+                                        <textarea rows="3" class="form-control" readonly><?= $data->keterangan_reject ?></textarea>
+                                    </div>
+                            <?php }
+                            } ?>
                             <?php if (isset($data->lampiran_1) || isset($data->lampiran_2) || isset($data->lampiran_3) || isset($data->lampiran_4) || isset($data->lampiran_5)) { ?>
                                 <div class="col-lg-12 mt-2">
                                     <label class="col-form-label">Lampiran Dokumen:</label>
